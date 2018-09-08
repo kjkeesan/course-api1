8d17598574c88c60d35e3065e6ea8fc2f73c1d23
Logeesan <kjkeesan@gmail.com>
2018-09-08 17:07:52 +0530

    ..

--- src/main/java/io/javabrains/springbootstarter/topics/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topics/TopicService.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootstarter.topics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,10 +8,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topics=Arrays.asList(
+	private List<Topic> topics=new ArrayList<>(Arrays.asList(
 			new Topic("1","Logeesan","First project"),
 			
-			new Topic("2","Logee","Second project"));
+			new Topic("2","Logee","Second project")));
 			
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -19,4 +20,9 @@ public class TopicService {
 	public Topic getTopic(String id){
 		return  topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
 	}
+	
+	public void addTopic(Topic topic){
+		topics.add(topic);
+	}
+
 }
